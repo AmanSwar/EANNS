9cb13c1fe3ac74f7ea32d8b9e065331436dac55b
Aman <aman.swar001@gmail.com>
2025-03-12 00:12:40 +0530

    vector as default storage

--- include/core/tensor.hpp
+++ include/core/tensor.hpp
@@ -1,12 +1,20 @@
 #pragma once
+#include <cstddef>
 #include <unordered_map>
 #include <string>
+#include <vector>
+
 
 namespace eanns{
 
-    struct Tensor{
-        float *vector;
-        std::unordered_map<std::string , std::string>* metadata;
-    };
-
+    class Tensor{
+        private:
+            std::vector<float> vector;
+            std::unordered_map<std::string , std::string> metadata;
+        public:
+            Tensor(
+                std::vector<float> vector , 
+                std::unordered_map<std::string , std::string> metadata = {}
+            ) : vector(vector) , metadata(metadata) {};          
+        };
 }
